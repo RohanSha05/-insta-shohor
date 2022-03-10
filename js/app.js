f10813af47bd41a6edc8d0b061ad1dfd74a9648a
RohanSha05 <rhnshariar@gmail.com>
2022-03-11 01:22:22 +0600

    commit03

--- js/app.js
+++ js/app.js
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -142,9 +142,9 @@ const showPosts = (posts) => {
     productsContainer.appendChild(div);
   });
 };
-
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  document.getElementById('liked').innerHTML = '';
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
@@ -153,12 +153,12 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  document.getElementById("reported").innerHTML = '';
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
 };
-// okay
 const loadPosts = async () => {
   let data = await fetch('../data/posts.json');
   posts = await data.json();
